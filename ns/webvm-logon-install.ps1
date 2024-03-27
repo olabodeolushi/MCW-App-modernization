cb16912268ce9a6b937cc21350e2ef0741f69921
Bode Olushi <bode.olushi@inwayz.ca>
2024-03-27 01:38:34 -0400

    Dev branch- changed folder path for various files

--- ns/webvm-logon-install.ps1
+++ ns/webvm-logon-install.ps1
@@ -25,25 +25,25 @@ Wait-Install
 Write-Host "Installing App Service Migration Assistant..."
 Start-Process -file 'C:\AppServiceMigrationAssistant.msi ' -arg '/qn /l*v C:\asma_install.txt' -passthru | wait-process
 
-# Install Edge
+# # Install Edge
+# Wait-Install
+# Write-Host "Installing Edge..."
+# Start-Process -file 'C:\MicrosoftEdgeEnterpriseX64.msi' -arg '/qn /l*v C:\edge_install.txt' -passthru | wait-process
+
+# Copy Web Site Files
 Wait-Install
-Write-Host "Installing Edge..."
-Start-Process -file 'C:\MicrosoftEdgeEnterpriseX64.msi' -arg '/qn /l*v C:\edge_install.txt' -passthru | wait-process
+Write-Host "Copying default website files..."
+Expand-Archive -LiteralPath "C:\MCW\MCW-App-modernization-$branchName\ns\web-deploy-files.zip" -DestinationPath 'C:\inetpub\wwwroot' -Force
+
+# Copy the database connection string to the web app.
+Write-Host "Updating config.json with the SQL IP Address and connection string information."
+Copy-Item "C:\MCW\MCW-App-modernization-$branchName\ns\config.json" -Destination 'C:\inetpub\wwwroot' -Force
 
 # Install .NET Core 3.1 SDK
 Wait-Install
 Write-Host "Installing .NET Core 3.1 SDK..."
 $pathArgs = {C:\dotnet-sdk-3.1.413-win-x64.exe /Install /Quiet /Norestart /Logs logCore31SDK.txt}
 Invoke-Command -ScriptBlock $pathArgs
-
-# Copy Web Site Files
-Wait-Install
-Write-Host "Copying default website files..."
-Expand-Archive -LiteralPath "C:\MCW\MCW-App-modernization-$branchName\Hands-on-lab\lab-files\web-deploy-files.zip" -DestinationPath 'C:\inetpub\wwwroot' -Force
-
-# Copy the database connection string to the web app.
-Write-Host "Updating config.json with the SQL IP Address and connection string information."
-Copy-Item "C:\MCW\MCW-App-modernization-$branchName\Hands-on-lab\lab-files\src\src\PartsUnlimitedWebsite\config.json" -Destination 'C:\inetpub\wwwroot' -Force
 
 Unregister-ScheduledTask -TaskName "Install Lab Requirements" -Confirm:$false
 
