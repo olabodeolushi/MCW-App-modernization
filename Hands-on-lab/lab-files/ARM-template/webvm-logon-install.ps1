cb7ddd5c0e5c0ceb6cfb7413ddbfe4232df1e2d0
Bode Olushi <bode.olushi@inwayz.ca>
2024-03-27 02:09:24 -0400

    Dev branch- changed folder path for various files

--- Hands-on-lab/lab-files/ARM-template/webvm-logon-install.ps1
+++ Hands-on-lab/lab-files/ARM-template/webvm-logon-install.ps1
@@ -19,7 +19,7 @@ function Wait-Install {
         Start-Sleep -Seconds 1
     }
 }
-$branchName = "main"
+$branchName = "dev"
 # Install App Service Migration Assistant
 Wait-Install
 Write-Host "Installing App Service Migration Assistant..."
